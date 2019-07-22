a7d05fe8bdab30bfee1a6615d1f3836081d69666
liam-ash <liam.ash@xero.com>
2019-07-22 14:32:30 +1000

    Got toastify working

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Header from "./common/Header";
 import HomePage from "./home/HomePage";
 import FileServerPage from "./fileserver/FileServer";
@@ -7,24 +7,21 @@ import ManageFileServerPage from "./fileserver/FileServerManagement";
 import ToastPage from "./toast/Toast";
 import PageNotFound from "./PageNotFound";
 import "./App.css";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   debugger;
   return (
     <div className="App">
       <Header />
-      <Router>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/toast" component={ToastPage} />
-          <Route path="/fileserver" component={FileServerPage} />
-          <Route
-            path="/fileservermanagement"
-            component={ManageFileServerPage}
-          />
-          <Route component={PageNotFound} />
-        </Switch>
-      </Router>{" "}
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/toast" component={ToastPage} />
+        <Route path="/fileserver" component={FileServerPage} />
+        <Route path="/fileservermanagement" component={ManageFileServerPage} />
+        <Route component={PageNotFound} />
+      </Switch>
+      <ToastContainer> autoClose={3000} hideProgressBar</ToastContainer>
     </div>
   );
 }
--- my-app/src/toast/Toast.js
+++ my-app/src/toast/Toast.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { toast } from "react-toastify";
 import "./toast.css";
+import "react-toastify/dist/ReactToastify.css";
 
 function ToastPage() {
   const toastClick = () => {
