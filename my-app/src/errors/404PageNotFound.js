38d4231c75e448e9931cc60506d0bfba9b7e04f8
liam-ash <liam.ash@xero.com>
2019-07-23 16:32:50 +1000

    Got the JSON server running on the app. Had to configure some settings first, as i'd forgotten to do so

--- my-app/src/errors/404PageNotFound.js
+++ my-app/src/errors/404PageNotFound.js
@@ -3,7 +3,7 @@ import NotFound from "./404PageNotFound.png";
 import "./404PageNotFound.css";
 
 const PageNotFound = () => (
-  <img src={NotFound} alt="Page Not Found!" class="center" />
+  <img src={NotFound} alt="Page Not Found!" className="center" />
 );
 
 export default PageNotFound;
