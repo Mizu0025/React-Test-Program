96d52f945117ecf49d508eb3ad70715039879fc3
liam-ash <liam.ash@xero.com>
2019-07-23 15:12:04 +1000

    coded api tools for the api server

---
+++ my-app/tools/mockData.js
@@ -0,0 +1,39 @@
+const courses = [
+  {
+    id: 1,
+    name: "Rosemary Gallows",
+    slug: "english",
+    gender: 2,
+    avatarURL: ""
+  },
+  {
+    id: 2,
+    title: "Fionne MacDough",
+    slug: "irish",
+    gender: 1,
+    avatarURL: ""
+  },
+  {
+    id: 3,
+    title: "Jeanne D'Orleans",
+    slug: "french",
+    gender: 1,
+    avatarURL: ""
+  }
+];
+
+const genders = [{ id: 1, name: "Male" }, { id: 2, name: "Female" }];
+
+const newUserDetails = {
+  id: null,
+  name: "",
+  gender: null,
+  avatarURL: ""
+};
+
+// Using CommonJS style export so we can consume via Node (without using Babel-node)
+module.exports = {
+  newCourse: newUserDetails,
+  courses,
+  genders
+};
